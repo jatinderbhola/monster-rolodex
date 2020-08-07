8cc3386adbcfce0676ec4d8ae51155176209e9e7
jatinder bhola <jatinderbhola@gmail.com>
2020-08-07 00:00:58 -0400

    card component + image + email ---  fixed

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./card.styles.css";
 export const Card = (props) => (
     <div className="card-container">
-        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`}></img>
-        <h1> {props.monster.name}</h1>
-        <h1> {props.monster.email}</h1>
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+        <h2> {props.monster.name}</h2>
+        <p> {props.monster.email}</p>
     </div>
 );
