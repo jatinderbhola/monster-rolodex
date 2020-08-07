e593e144b0a6c64ef8e75775b616305472bc82cf
jatinder bhola <jatinderbhola@gmail.com>
2020-08-06 23:02:02 -0400

    basic JSX flow - state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor() {
+    super(); // call the super in componet 
+
+    // this.state exist
+    this.state = {
+      monsters: [{
+        name: "satish"
+      }, {
+        name: "tarun"
+      }, {
+        name: "shital"
+      }, {
+        name: "muthu"
+      }]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p> {this.state.string} </p>
+
+          <button onClick={() => this.setState({ string: 'hello Poonam' })} >click me</button>
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div >
+    );
+  }
 }
+
 
 export default App;
