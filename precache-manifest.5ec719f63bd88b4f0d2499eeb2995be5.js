866777d1b003e445bb681d8356c40b88d27c66cb
jatinder bhola <jatinderbhola@gmail.com>
2020-08-09 15:06:01 -0400

    Updates

---
+++ precache-manifest.5ec719f63bd88b4f0d2499eeb2995be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211d938e11b36b3cf871700098f1b6dd",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "dab57bdbf5bc58ab7ebd",
+    "url": "/monster-rolodex/static/css/main.b75c6fe4.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab57bdbf5bc58ab7ebd",
+    "url": "/monster-rolodex/static/js/main.0cec0118.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
