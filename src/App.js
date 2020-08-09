5df13388a6ed8426143ab57e0f0b0d8abf1692e3
jatinder bhola <jatinderbhola@gmail.com>
2020-08-09 12:46:26 -0400

    setState callback

--- src/App.js
+++ src/App.js
@@ -13,8 +13,11 @@ class App extends Component {
       monsters: [
         { id: 1, name: "Satish" }, { id: 2, name: "Satish" },
         { id: 3, name: "Satish" }, { id: 4, name: "Satish" }
-      ]
+      ],
+      searchValue: ''
     }
+
+    this.filterMonster = (e) => { console.log(e); }
 
   }
   // mounting: react put comp. on the page, render first time on the view 
@@ -29,12 +32,14 @@ class App extends Component {
     return (
 
       <div className="App">
+        <input type="search" placeholder="search monster" onChange={e => this.setState({ searchValue: e.target.value }, () => console.log(this.state))} />
         <CardList monsters={this.state.monsters} >
 
         </CardList>
       </div >
     );
   }
+
 }
 
 
